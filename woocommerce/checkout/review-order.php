01f40c61000b0fadf4adfccfe14f505e17908cc9
Chregi Glatthard <chregi.glatthard@gmail.com>
2015-01-03 00:16:29 +0100

    design adjustments checkout, order review & footer

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -114,6 +114,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	<?php do_action( 'woocommerce_review_order_before_payment' ); ?>
 
 	<div id="payment">
+		<h3><?php _e( 'Payment', 'woocommerce' ); ?></h3>
 		<?php if ( WC()->cart->needs_payment() ) : ?>
 		<ul class="payment_methods methods">
 			<?php
@@ -166,12 +167,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 			<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
-			<?php
-			$order_button_text = apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) );
-
-			echo apply_filters( 'woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" />' );
-			?>
-
+			<!-- Terms & Conditions -->
 			<?php if ( wc_get_page_id( 'terms' ) > 0 && apply_filters( 'woocommerce_checkout_show_terms', true ) ) { 
 				$terms_is_checked = apply_filters( 'woocommerce_terms_is_checked_default', isset( $_POST['terms'] ) );
 				?>
@@ -180,6 +176,14 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 					<input type="checkbox" class="input-checkbox" name="terms" <?php checked( $terms_is_checked, true ); ?> id="terms" />
 				</p>
 			<?php } ?>
+
+			<?php
+			$order_button_text = apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) );
+
+			echo apply_filters( 'woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" />' );
+			?>
+
+			
 
 			<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
