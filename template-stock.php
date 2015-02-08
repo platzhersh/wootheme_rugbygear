4d3ca27684d4619545c51db7c32990a3e9c2481b
Chregi Glatthard <chregi.glatthard@gmail.com>
2015-02-08 09:09:25 +0100

    add 'download as csv' button

--- template-stock.php
+++ template-stock.php
@@ -128,9 +128,14 @@ if (isset($_GET["download"])) {
 <div class="container">
 		<header>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<h1 class="title"><?php the_title(); ?></h1>
+			<h1 class="title"><?php the_title(); ?>
+				<a href="?download"><span class="btn btn-primary pull-right">
+					<span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download as CSV</span>
+				</a>
+			</h1>
 			<?php the_content(); ?>
 		<?php endwhile; endif; ?>
+
 	</header>
 	<section>
 		<?php
